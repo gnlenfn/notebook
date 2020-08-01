59e8417b50408f52e458f789afc0b59c49157c3b
Yun, Hong In <akenfn513@gmail.com>
2020-08-01 12:20:48 +0900

    Update 다리를지나는트럭.py

--- week_02/yoon/Programmers/다리를지나는트럭.py
+++ week_02/yoon/Programmers/다리를지나는트럭.py
@@ -1,10 +1,20 @@
 def solution(bridge_length, max_weight, truck_weights):
-    bridge = [0] * bridge_length
-    time = 0
-    cur_weight = 0
-
-    while truck_weights:
-        cur = truck_weights.pop(0)
-        time += 1
-        cur_weight -= cur
-        
+    # FIFO 문제.
+    bridge = [0]*bridge_length
+    curr_weight = 0
+    ans = 0
+    while len(truck_weights) > 0:
+        ans += 1
+        ar = bridge.pop(0)
+        curr_weight -= ar
+        if curr_weight + truck_weights[0] > max_weight:
+            bridge.append(0)            
+        else:
+            truck = truck_weights.pop(0)
+            bridge.append(truck)
+            curr_weight += truck
+    while curr_weight > 0:
+        ans += 1
+        ar = bridge.pop(0)
+        curr_weight -= ar
+    return ans
